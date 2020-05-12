678fe20acedb0a58740acc9d4fcac5a920f2ac09
None <None>
2020-05-11 21:05:01 -0700

    last update

--- mystartup.py
+++ mystartup.py
@@ -3,7 +3,7 @@
 def launch():
 
 	from pox.log.level import launch
-	launch(DEBUG=True)
+	launch(DEBUG=False)
 
 	from pox.samples.pretty_log import launch
 	launch()
@@ -11,8 +11,8 @@ def launch():
 	from myl2_learning import launch
 	launch(ignore="1")
 
-	from mymessaging_ip_loadbalancer import launch
-	launch(ip1="10.0.1.1",ip2="10.0.2.1",servers="10.0.0.2,10.0.0.3,10.0.0.4,10.0.0.5")
+	from myip_loadbalancer import launch
+	launch(ip1="10.0.1.1",ip2="10.0.2.1",servers="10.0.0.2,10.0.0.3,10.0.0.4,10.0.0.5,10.0.0.6")
 	
 	from messenger.tcp_transport import launch
         launch()
